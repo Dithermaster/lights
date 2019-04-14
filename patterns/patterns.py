50a6eb27ca1606708db8e979c068e2a9bb8f1d60
Dennis Adams <dennis.adams@sony.com>
2019-04-14 15:09:36 -0500

    remove conditional again

--- patterns/patterns.py
+++ patterns/patterns.py
@@ -60,10 +60,7 @@ def ball_spotlight(led_theta, ball_rho, ball_theta, day_ms, rotation):
     if (angle_diff < 0.0):
         angle_diff = -angle_diff
     angle_diff = angle_diff - math.floor(angle_diff / TWO_PI) * TWO_PI
-    w = 0
-    if ((angle_diff - TWO_PI/25) < (1.0-ball_rho) * (TWO_PI / 2.0)):
-        bright = min(10.0*((1.0-ball_rho) * (TWO_PI / 2.0) - (angle_diff - TWO_PI/25)),1.0)
-        w = max(bright, 0.3)
+    w = max(min(10*((1.0-ball_rho) * (TWO_PI / 2.0) - (angle_diff - TWO_PI/25)), max(ball_rho,0.3)), 0.0)
     return Perceptual_to_RGBW(w, w, w)
 
 # sisbot simulator - replace with code that gets ball location from sisbot (I could not get that working, so I'm simulating it)
