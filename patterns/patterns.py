10c7407259f55e806d2c5a103a8157c07fc0dec5
Dennis Adams <dennis.adams@sony.com>
2019-04-14 15:59:11 -0500

    pattern 3

--- patterns/patterns.py
+++ patterns/patterns.py
@@ -72,7 +72,7 @@ def ball_spotlight(led_theta, ball_rho, ball_theta, day_ms, rotation):
 
 # sisbot simulator - replace with code that gets ball location from sisbot (I could not get that working, so I'm simulating it)
 def sisbotSimulator():
-    pattern = 2
+    pattern = 3
     dt = datetime.now()
     day_ms = ((dt.hour * 60 + dt.minute) * 60 + dt.second) * 1000 + dt.microsecond / 1000
     speed = 15 # 0=stopped, 1=slow (1 minute per rotation), 60=fast (1 second per rotation)
